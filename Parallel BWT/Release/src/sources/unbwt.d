5f8b6869ec7d3faa24cbadd3edb6c15f96b3bdd9
stevalla <32373697+stevalla@users.noreply.github.com>
2019-02-25 20:14:44 +0100

    Delete unbwt.d

--- Parallel BWT/Release/src/sources/unbwt.d
+++
@@ -1,6 +0,0 @@
-src/sources/unbwt.o: ../src/sources/unbwt.c \
- ../src/sources/../headers/unbwt.h ../src/sources/../headers/util.h
-
-../src/sources/../headers/unbwt.h:
-
-../src/sources/../headers/util.h:
