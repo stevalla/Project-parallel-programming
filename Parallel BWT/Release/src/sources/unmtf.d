c3c245f86552bcf9ab469fff17345ae4fec43646
stevalla <32373697+stevalla@users.noreply.github.com>
2019-02-25 20:14:56 +0100

    Delete unmtf.d

--- Parallel BWT/Release/src/sources/unmtf.d
+++
@@ -1,9 +0,0 @@
-src/sources/unmtf.o: ../src/sources/unmtf.c \
- ../src/sources/../headers/unmtf.h ../src/sources/../headers/mtf.h \
- ../src/sources/../headers/util.h
-
-../src/sources/../headers/unmtf.h:
-
-../src/sources/../headers/mtf.h:
-
-../src/sources/../headers/util.h:
