db4620455a450c8648e9e2669c218349b7c2bb6b
stevalla <32373697+stevalla@users.noreply.github.com>
2019-02-25 20:14:20 +0100

    Delete suffixTree.d

--- Parallel BWT/Release/src/sources/suffixTree.d
+++
@@ -1,7 +0,0 @@
-src/sources/suffixTree.o: ../src/sources/suffixTree.c \
- ../src/sources/../headers/suffixTree.h \
- ../src/sources/../headers/../extern/uthash.h
-
-../src/sources/../headers/suffixTree.h:
-
-../src/sources/../headers/../extern/uthash.h:
