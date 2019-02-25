8cce157558f763b664603cd8b6e1dd2b58f7355f
stevalla <32373697+stevalla@users.noreply.github.com>
2019-02-25 20:13:11 +0100

    Delete arith.d

--- Parallel BWT/Release/src/sources/arith.d
+++
@@ -1,6 +0,0 @@
-src/sources/arith.o: ../src/sources/arith.c \
- ../src/sources/../headers/arith.h ../src/sources/../headers/util.h
-
-../src/sources/../headers/arith.h:
-
-../src/sources/../headers/util.h:
