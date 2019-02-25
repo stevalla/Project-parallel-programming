cfe82bdfbb4e87413b0ad6d9cb829bc4e8fe37f7
stevalla <32373697+stevalla@users.noreply.github.com>
2019-02-25 20:14:31 +0100

    Delete unarith.d

--- Parallel BWT/Release/src/sources/unarith.d
+++
@@ -1,9 +0,0 @@
-src/sources/unarith.o: ../src/sources/unarith.c \
- ../src/sources/../headers/unarith.h ../src/sources/../headers/arith.h \
- ../src/sources/../headers/util.h
-
-../src/sources/../headers/unarith.h:
-
-../src/sources/../headers/arith.h:
-
-../src/sources/../headers/util.h:
